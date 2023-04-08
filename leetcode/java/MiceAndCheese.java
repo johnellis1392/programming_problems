c92ad55e01a2f48547357542dd6162fb9c156b61
johnellis1392 <john.ellis1392@gmail.com>
2023-04-07 20:35:39 -0700

    2023/04/07 LeetCode MiceAndCheese Added a couple more implementations but still havent quite solved it

--- leetcode/java/MiceAndCheese.java
+++ leetcode/java/MiceAndCheese.java
@@ -71,7 +71,7 @@ public class MiceAndCheese {
     return this.miceAndCheese_helper(a, k, 0, 0, 0);
   }
 
-  public int miceAndCheese(final int[] in1, final int[] in2, final int k) {
+  public int miceAndCheese_v2(final int[] in1, final int[] in2, final int k) {
     int[] indices = new int[k];
     for (int i = 0; i < k; i++) indices[i] = i;
 
@@ -104,18 +104,87 @@ public class MiceAndCheese {
     return result;
   }
 
+  // Solution 3
+  // I realized here that I can memoize the whole in2 computation by
+  // pre-calculating the sum of the second array and just selectively
+  // subtracting the elements corresponding to the chosen indices for
+  // the current combination from the sum and adding the in1 values.
+  // It's faster because we can remove a factor of n, but the time-complexity
+  // is still the complexity of the combinations algorithm multiplied by k.
+  // According to the StackOverflow article I saw it in, the combinations
+  // time is O(choose(n, k) * k), so the resultant complexity is probably
+  // O(choose(n, k) * k^2) what with the second k-iteration.
+  public int miceAndCheese_v3(final int[] in1, final int[] in2, final int k) {
+    int result = 0,
+        in2Sum = 0;
+    for (int i : in2) in2Sum += i;
+    if (k == 0) return in2Sum;
+
+    int[] indices = new int[k];
+    for (int i = 0; i < k; i++) indices[i] = i;
+
+    final int n = in1.length;
+    while (indices[k-1] < n) {
+      int sum = in2Sum;
+      for (int i = 0; i < k; i++) {
+        sum = sum - in2[indices[i]] + in1[indices[i]];
+      }
+      result = Math.max(result, sum);
+
+      int t = k-1;
+      while (t != 0 && indices[t] == n-k+t) t--;
+      indices[t]++;
+      for (int i = t+1; i<k; i++) indices[i] = indices[i-1]+1;
+    }
+    return result;
+  }
+
+  // Solution 4
+  // Greedy. I don't know if this will work, because there are potentially
+  // some use-cases where a greedy algorithm will skip valid outcomes, but
+  // it's worth a shot.
+  //
+  // ...it fails on case 3. Greedy algorithm does not work.
+  public int miceAndCheese(final int[] in1, final int[] in2, final int k) {
+    int[] indices = new int[k];
+    for (int i = 0; i < k; i++) indices[i] = i;
+    for (int i = k; i < in1.length; i++) {
+      int minIndex = -1;
+      for (int j = 0; j < k; j++) {
+        if (in1[indices[j]] < in1[i] && (minIndex == -1 || in1[indices[j]] < in1[indices[minIndex]]))
+          minIndex = j;
+      }
+      if (minIndex != -1) indices[minIndex] = i;
+    }
+    dump(indices);
+    
+    int j = 0;
+    int result = 0, result2 = 0;
+    for (int i = 0; i < in2.length; i++) {
+      if (j < k && i == indices[j]) {
+        System.out.printf("j=%d, i=%d\n", j, i);
+        j++;
+        result += in1[i];
+      } else {
+        result2 += in2[i];
+      }
+    }
+    System.out.printf("result1=%d, result2=%d\n", result, result2);
+    return result + result2;
+  }
+
   public static void runTests() {
     var m = new MiceAndCheese();
     var tests = new Test[] {
       new Test(new int[]{1,1,3,4}, new int[]{4,4,1,1}, 2, 15),
       new Test(new int[]{1,1}, new int[]{1,1}, 2, 2),
       new Test(new int[]{1,4,4,6,4}, new int[]{6,5,3,6,1}, 1, 24),
-      new Test(
-        new int[]{54,59,94,87,32,10,55,44,21,73,12,70,89,49,13,34,78,20,20,75,90,44,48,74,78,32,70,76,79,49,50,69},
-        new int[]{78,43,69,22,32,67,65,38,51,4,21,27,82,61,12,85,62,60,67,16,22,3,5,16,13,35,13,41,72,85,20,54},
-        17,
-        2053
-      )
+      // new Test(
+      //   new int[]{54,59,94,87,32,10,55,44,21,73,12,70,89,49,13,34,78,20,20,75,90,44,48,74,78,32,70,76,79,49,50,69},
+      //   new int[]{78,43,69,22,32,67,65,38,51,4,21,27,82,61,12,85,62,60,67,16,22,3,5,16,13,35,13,41,72,85,20,54},
+      //   17,
+      //   2053
+      // )
     };
     for (var t : tests) {
       var a = m.miceAndCheese(t.in1, t.in2, t.k);
@@ -125,6 +194,7 @@ public class MiceAndCheese {
         System.out.println("SUCCESS");
       }
     }
+    System.out.println("Done.");
   }
 
   public static void main(String[] args) {
