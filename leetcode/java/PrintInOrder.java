54cb690a5d3f8bd01592959dd5f660ba4615703f
John Ellis <john.ellis1392@gmail.com>
2023-04-11 12:26:23 -0700

    2023/04/11 LeetCode PrintInOrder

--- leetcode/java/PrintInOrder.java
+++ leetcode/java/PrintInOrder.java
@@ -1,4 +1,6 @@
 import java.util.concurrent.Executors;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
 
 public class PrintInOrder {
   public class Solution1 {
@@ -35,6 +37,89 @@ public class PrintInOrder {
 
     public void third(Runnable printThird) throws InterruptedException {
       while (!this.isSecondDone()) Thread.yield();
+      printThird.run();
+    }
+  }
+
+  // LeetCode solution using CountDownLatch's
+  public class Solution2 {
+    private CountDownLatch waitForFirst;
+    private CountDownLatch waitForSecond;
+    public Solution2() {
+      this.waitForFirst = new CountDownLatch(1);
+      this.waitForSecond = new CountDownLatch(1);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+      printFirst.run();
+      waitForFirst.countDown();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+      waitForFirst.await();
+      printSecond.run();
+      waitForSecond.countDown();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+      waitForSecond.await();
+      printThird.run();
+    }
+  }
+
+  // Another solution using wait and notify
+  public class Solution3 {
+    private static int flag = 1;
+    public Solution3() {}
+
+    public synchronized void first(Runnable printFirst) throws InterruptedException {
+      while (flag != 1) wait();
+      flag = 2;
+      printFirst.run();
+      notifyAll();
+    }
+
+    public synchronized void second(Runnable printSecond) throws InterruptedException {
+      while (flag != 2) wait();
+      flag = 3;
+      printSecond.run();
+      notifyAll();
+    }
+
+    public synchronized void third(Runnable printThird) throws InterruptedException {
+      while (flag != 3) wait();
+      flag = 1;
+      printThird.run();
+      notifyAll();
+    }
+  }
+
+  // Solution using Semaphores
+  // I don't think the while loops here are the best solution, because
+  // they'll just run infinitely, so while it's fast, I think using
+  // Thread.yield() would be more efficient.
+  public class Solution4 {
+    Semaphore waitForFirst;
+    Semaphore waitForSecond;
+
+    public Solution4() {
+      waitForFirst = new Semaphore(0);
+      waitForSecond = new Semaphore(0);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+      printFirst.run();
+      waitForFirst.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+      while (!waitForFirst.tryAcquire());
+      printSecond.run();
+      waitForSecond.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+      while (!waitForSecond.tryAcquire());
       printThird.run();
     }
   }
